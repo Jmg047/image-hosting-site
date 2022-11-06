45a202102e9165da9942385703223e7913180da5
Jmg047 <90755835+Jmg047@users.noreply.github.com>
2022-11-06 06:00:39 -0800

    "milestone 2"

--- application/routes/index.js
+++ application/routes/index.js
@@ -6,4 +6,25 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'CSC 317 App', name:"Jaime Guardado" });
 });
 
+router.get("/index",function(req, res){
+  res.render('index');
+});
+
+router.get("/login",function(req, res){
+  res.render('login');
+});
+
+router.get("/postimage",function(req, res){
+  res.render('postimage');
+});
+
+router.get("/registration",function(req, res){
+  res.render('registration');
+});
+
+router.get("/viewpost",function(req, res){
+  res.render('viewpost');
+});
+
 module.exports = router;
+
